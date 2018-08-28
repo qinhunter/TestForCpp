66bc9d00218bd81ff255ad7dcd712fcfa58af977
qinweite <qinweite@kuaishou.com>
2018-08-28 17:12:03 +0800

    add the test of modifying in data storaged in .rodata section

--- const_test.c
+++ const_test.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-int main() {
+void test1(void) {
+
 	const char c = 'c';
 	char* pc;
 	pc = &c;
@@ -9,14 +10,35 @@ int main() {
 	*pcc = &c;//same as	pc = &c
 
 	*pc = 'C';  //2: modifies a const object
-	// const char b = 'b';
-	// char* pb;
-	// pb = &b;// warning
-	// printf("*pb = %c, char b = %c \n", *pb, b);
-	// *pb = 'a';
-	// printf("*pb = %c, char b = %c \n", *pb, b);
-	// // b = 'd'; error
 
+}
+
+void test2(void) {
+
+	const char b = 'b';
+	char* pb;
+	pb = &b;// warning
+	printf("*pb = %c, char b = %c \n", *pb, b);
+	*pb = 'a';
+	printf("*pb = %c, char b = %c \n", *pb, b);
+	// b = 'd';//this will be error
+
+}
+
+void test3(void) {
+	
+	char *header = "abcdef";//the char string is const and is saved in .rodata section which is can only be read and not write 
+	char *p = header;
+	printf("*p = %c, header = %s \n", *p, header);
+	*p = 'H';//Run error: Bus error: 10
+	printf("*p = %c, header = %s \n", *p, header);
+
+}
+
+int main() {
+	test1();
+	test2();
+	test3();
 
 	return 0;
 }
