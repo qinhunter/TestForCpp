a96859db3c936a8321cb01b7f496b76f5b3671ff
qinweite <qinweite@baidu.com>
2021-03-01 20:58:13 +0800

    the key of set could not be pair

---
+++ not_allow/test_the_key_of_set.cc
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <stdlib.h> 
+#include <time.h>  
+#include <unordered_set>
+
+using namespace std;  
+int main() 
+{  
+    std::unordered_set<std::pair<int, int>> count;
+    return 0; 
+}
