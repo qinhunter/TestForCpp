ae36b307e0caee59a71ef69a2ed21fdd0fb0f96c
qinweite <qinweite@baidu.com>
2021-05-10 19:34:16 +0800

    how to construct in map

--- map.cc
+++ map.cc
@@ -4,10 +4,30 @@
 #include<map>
 
 using namespace std;  
+class Node {
+public:
+    Node() : val(-1) {
+        std::cout << "default constructor val: " << val << std::endl;
+    }
+    Node(int value) : val(value) {
+        std::cout << "constructor val: " << val << std::endl;
+    }
+    Node(const Node& node) {
+        val = node.val + 1;
+        std::cout << "copy constructor val: " << val << std::endl;
+    }
+    Node& operator=(const Node& node) {
+        val = node.val + 2;
+        std::cout << "opeartor= val: " << val << std::endl;
+        return *this;
+    }
+    int val = 0;
+};
 int main() 
 {  
-    std::map<int, std::pair<int, int>> a;
-    int& b = a[1].first = 2;
-    std::cout << 1;
+    std::map<int, Node> a;
+    Node node(1);
+    a[1] = node;
+    Node new_node(a[1]);
     return 0;
 }
