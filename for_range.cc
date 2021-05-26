e87ecb2b0fd182ce2f667ec527569bb3f4900d8c
qinweite <qinweite@baidu.com>
2021-05-26 19:49:43 +0800

    update for range and shared_ptr

---
+++ for_range.cc
@@ -0,0 +1,46 @@
+#include <iostream> 
+#include <vector>
+#include <string>
+
+
+
+using namespace std;  
+
+class Testclass {
+public:
+	Testclass(int num = 0) : _num(num) {
+		cout << this << " constructor " << to_string(_num) << endl;
+	}
+
+	Testclass(const Testclass& other) : _num(other._num) {
+		cout << this << " copy constructor " << to_string(_num) << endl;
+	}
+
+	Testclass(Testclass&& other) : _num(other._num) {
+		cout << this << " move copy constructor " << to_string(_num) << endl;
+	}
+
+	~Testclass() {
+		cout << this << " destructor " << to_string(_num) << endl;
+	}
+	int _num;
+
+};
+
+int main(int argc, char* argv[]) {
+	vector<Testclass> testclass_vec;
+	testclass_vec.reserve(10);// Without reserving, push_back will resize the vector for many times, which will influence the efficiency and the most important is that it will construct or destruct element in the vector.
+    for (int i = 0; i < 10; ++i) {
+	    testclass_vec.emplace_back(i);
+    }
+    std::cout << "show vector\n";
+    for (const auto& tmp : testclass_vec) {
+        std::cout << &tmp << ": " << to_string(tmp._num) << std::endl;
+    }
+    std::cout << "modify vector\n";
+    for (auto&& tmp : testclass_vec) {
+        tmp._num += 10;
+        std::cout << &tmp << ": " << std::to_string(tmp._num) << std::endl;
+    }
+	return 0; 
+}
--- shared_ptr.cc
+++ shared_ptr.cc
@@ -4,9 +4,40 @@
 #include <time.h>  
 
 using namespace std;  
-int main() 
-{  
-    std::shared_ptr<std::string> shp = std::shared_ptr<std::string>(nullptr);
-    std::cout << "done" << std::endl;
-    return 0; 
+
+struct Node {
+    Node(const int& __val = 0) : val(__val) {}
+    operator bool() {
+        return val != 0;
+    }
+    int val = 0;
+};
+
+int main()
+{
+    // std::shared_ptr<std::string> shp = std::shared_ptr<std::string>(nullptr);
+    // std::cout << "done" << std::endl;
+    std::shared_ptr<std::string> a;
+    auto b = a;
+    std::cout << a.use_count() << std::endl;
+    std::cout << b.use_count() << std::endl;
+    std::cout << std::to_string(a == nullptr) << std::endl;
+    if (a) {
+        std::cout << "if a" << std::endl;
+    } else {
+        std::cout << "not if a" << std::endl;
+    }
+    Node n;
+    if (n) {
+        std::cout << "if n" << std::endl;
+    } else {
+        std::cout << "not if n" << std::endl;
+    }
+    Node n1(1);
+    if (n1) {
+        std::cout << "if n1" << std::endl;
+    } else {
+        std::cout << "not if n1" << std::endl;
+    }
+    return 0;
 }
