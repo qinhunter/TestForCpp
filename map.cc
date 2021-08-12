1cc8ae58cf7dc9bb7370448cd300c7c2a4261a14
qinweite <qinweite@baidu.com>
2021-08-12 20:57:10 +0800

    update

--- map.cc
+++ map.cc
@@ -32,5 +32,20 @@ int main()
     Node new_node(a[1]);
     // key could not be set as vector<int> !!!
     // std::unordered_map<std::vector<int>, std::vector<int>> vec_to_vec_map;
+    std::unordered_map<int, int> map1;
+    map1.reserve(1000);
+    std::unordered_map<int, int> map2;
+    map2.reserve(100);
+    std::cout << "empty map1 equals empty map2: " << (map1 == map2) << std::endl;
+    map1[1] = 2;
+    std::cout << "inserted map1 equals map2: " << (map1 == map2) << std::endl;
+    map2[1] = 2;
+    std::cout << "map1 equals inserted map2: " << (map1 == map2) << std::endl;
+    map1[2] = map2[2] = 3;
+    map1[20000001] = map2[20000001] = 3;
+    map1[200000001] = map2[200000001] = 3;
+    map1[2000000001] = map2[2000000001] = 3;
+    map1[100000000001] = map2[100000000001] = 3;
+    std::cout << "map1 equals map2: " << (map1 == map2) << std::endl;
     return 0;
 }
