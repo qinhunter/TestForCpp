fed8b62a24b3a952f58a9ddb50bd9c00e675763c
qinweite <qinweite@baidu.com>
2022-07-19 13:17:29 +0800

    test class init sequeence

--- class_init.cc
+++ class_init.cc
@@ -5,12 +5,21 @@ int add_one(int v) {
 }
 class A {
 public:
-    A(int val_) : val(add_one(val_)) {}
+    A(int val_) : val(add_one(val_)) {
+        printf("val %d\n", val_);
+    }
     int val;
 };
 //Test about struct and typedef
+//
+class B : public A {
+public:
+    B(int val) : a(1), A(val) {}
+    A a;
+};
 
 int main() {
-    A a(1);
+    // A a(1);
+    B b(3);
 	return 0;
 } 
