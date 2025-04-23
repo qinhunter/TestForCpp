f0fbe560d27dff9bf970ef793ac9c52fa4cfd753
qinweite <qinweite@baidu.com>
2025-04-23 11:45:34 +0800

    const_member_function_modify_non_const_member.cc

---
+++ const_member_function_modify_non_const_member.cc
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <stdlib.h> 
+#include <time.h>  
+
+using namespace std;  
+class Base {
+public:
+    void update(const int& v) {
+        b = v;
+    }
+    int b = 0;
+};
+
+class Test{
+    public:
+        Test() {
+            base = new Base();
+        }
+        ~Test() {
+            delete base;
+        }
+        void show() const {
+            std::cout << "a = " << a << ", base->b = " << base->b << std::endl;
+        }
+        void modify_member() const {
+            std::cout << "start modify_member" << std::endl;
+            show();
+            base->b = 2;
+            std::cout << "after set base->b" << std::endl;
+            show();
+            base->update(3);
+            std::cout << "after call base->update" << std::endl;
+            show();
+        }
+    private:
+        int a = 0;
+        Base* base = nullptr;
+};
+int main() 
+{  
+    Test test;
+    test.modify_static_member();
+}
