6111e8db15dfaf14fe88f04387288f8f09327a8d
qinweite <qinweite@baidu.com>
2025-12-24 16:57:22 +0800

    update

--- static_symbol/main_use_b.cpp
+++ static_symbol/main_use_b.cpp
@@ -3,8 +3,8 @@
 #include "B.h"
 
 void register_func() {
-    std::unique_ptr<B> b(new B());
-    // b->show();
+    std::unique_ptr<B> b(new B()); // only when b realizes virtual function, this way works.
+    // b->show(); // if b does not realize virtual function, explicitly using B's function also runs and may be the only way.
 }
 int main() {
     Registry::instance().dump();
